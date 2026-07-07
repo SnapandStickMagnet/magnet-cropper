a2e4b06b12158caae0aa52b944e5796357889c24
SnapandStickMagnet <jtwenger3@gmail.com>
2026-07-07 16:37:41 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let globalBase64Data = "";
 // Replace the placeholder link below with your actual Google Web App URL.
 // Ensure your link retains the quotes around it, ending with "/exec".
 // =========================================================================
-const GOOGLE_SCRIPT_URL = "https://drive.google.com/drive/folders/17yX06W7LCJ39WmxvVpvwi3CmzroCSnbz";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwGJ4Q33F-95EUGtcn6XiTj9BoMbospvGnJgwpHuWvBknJY_3eSdndZ9-kV1VDljpZ87g/exec";
 
 
 const imageInput = document.getElementById('imageInput');
